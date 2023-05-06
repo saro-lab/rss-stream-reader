f0db7cfd261e231f6f37832d2a6f3c722b08522b
PARK Yong Seo <j@saro.me>
2023-05-06 23:18:15 +0900

    fake userAgent

---
+++ src/main/kotlin/me/saro/rss/Globals.kt
@@ -0,0 +1,11 @@
+package me.saro.rss
+
+import java.io.InputStream
+import java.net.URL
+
+const val USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
+
+fun URL.openStreamWithAgent(): InputStream =
+    this.openConnection().apply {
+        //setRequestProperty("User-Agent", USER_AGENT)
+    }.getInputStream()
